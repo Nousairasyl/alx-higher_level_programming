476d182aa6b9b92d28fc8981b4ce9fb5b7e659f3
Nousairasyl <nousair.asyl@gmail.com>
2023-05-30 18:11:04 -0700

    main

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,4 +1,5 @@
-"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
+#!/usr/bin/python3
+"""Define a MagicClass that does exactly as the bytecode provided."""
 
 import math
 
@@ -8,6 +9,7 @@ class MagicClass:
 
     def __init__(self, radius=0):
         """Initialize a MagicClass.
+
         Arg:
             radius (float or int): The radius of the new MagicClass.
         """

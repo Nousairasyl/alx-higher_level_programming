8e91c075a80dfeb4a5906d1438a89ae721a09573
Nousairasyl <nousair.asyl.com>
2024-01-14 17:42:06 -0800

    hjdj

--- 0x15-javascript-web_jquery/6-script.js
+++
@@ -1,9 +0,0 @@
-// Wait for the document to be ready
-$(document).ready(
-  function () {
-      $('DIV#update_header').click(
-        function () {
-            // Update the text of the header
-            $('header').text('New get header!!!');
-        });
-});
--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,9 +0,0 @@
-// Wait for the document to be ready
-$("document").ready(function () {
-    // Update the `character` text
-    // Get the name from the API URL
-    const URL = "https://swapi-api.alx-tools.com/api/people/5/?format=json";
-    $.get(URL, function (data) {
-        $("#character").text(data.name);
-    });
-});
